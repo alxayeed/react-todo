1046a2b86036e0fe7520ccc13637632ed4812fda
al <alxayeed@gmail.com>
2020-12-15 16:39:30 +0600

    animation added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ListItems from './listItems';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
+
 library.add(faTrash)
 
 class App extends React.Component {
